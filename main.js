c1dd13d9f60eedd5fa9f6ccce4d8a91194c36c75
giovdigua <giovdigua@gmail.com>
2019-12-03 18:39:12 +0100

    Milestone 2 principal point complited

--- main.js
+++ main.js
@@ -1,1 +1,98 @@
+$(document).ready(function() {
 
+
+    //invio del messaggio
+    //intercetto il click
+    $('.right-footer-icon.f-right').click(function() {
+        invia_messaggio();
+        //richiamo la funzione di risposta automatica dopo 3 secondi
+        setTimeout(answer , 3000);
+    });
+
+    //intercetto il tasto invio
+    $('.new-message-inputs').keypress(function(event) {
+        //controllo se il tasto digitato corrisponde al tasto enter
+        if (event.which == 13) {
+            invia_messaggio();
+            //richiamo la funzione di risposta automatica dopo 3 secondi
+            setTimeout(answer , 3000);
+        }
+    });
+
+    //Intercetto i tasti digitati nell'input
+    $('.new-message-inputs').keyup(function() {
+        //recupero il testo inserito nel messaggio
+        var testo_messaggio = $('.new-message-inputs').val();
+        if (testo_messaggio.length != 0) {
+            //Se il messaggio non è vuoto , mostro l'icona dell'aereo e nascondo il microfono
+            $('.right-footer-icon.f-right i').removeClass('fa fa-microphone').addClass('fas fa-paper-plane');
+        }else {
+            //altrimenti (il messaggio è vuoto),nascondo l'icona dell'aereo e mostro il microfono
+            $('.right-footer-icon.f-right i').addClass('fa fa-microphone').removeClass('fas fa-paper-plane');
+        }
+    });
+
+    // //intercetto il focus nell'area di testo del messaaggio
+    // $('.new-message-inputs').focus(function() {
+    //     $('.right-footer-icon.f-right i').toggleClass('fa fa-microphone fas fa-paper-plane');
+    // })
+
+    //intercetto l'uscita del focus dall'area di testo del messaaggio
+    $('.new-message-inputs').blur(function() {
+        $('.right-footer-icon.f-right i').toggleClass('fa fa-microphone fas fa-paper-plane');
+    })
+
+
+    //Ricerca se un utente è presente nella lista
+    $('#contacts-filter').keyup(function(event) {
+        var testo_ricerca = $('#contacts-filter').val();
+        console.log(testo_ricerca);
+        if (testo_ricerca.length != 0) {
+            $('.contact').each(function() {
+                var elemento_corrente = $(this).find('.contact-name').text();
+                console.log(elemento_corrente);
+                if (elemento_corrente.toLowerCase().includes(testo_ricerca.toLowerCase())) {
+                    $(this).show();
+                }
+                else{
+                    $(this).hide();
+                }
+
+            });
+        }
+
+    });
+
+
+});
+
+function invia_messaggio() {
+    //Recupero il contenuto dell'input del messaggio
+    var testo_messaggio = $('.new-message-inputs').val();
+    //se il messaggio non è vuoto
+    if (testo_messaggio.length != 0) {
+        //clono il template del invia_messaggio
+        var nuovo_messaggio = $('.template .message').clone();
+        //inserisco nel giusto span il testo del invia_messaggio
+        nuovo_messaggio.children('.message-text').text(testo_messaggio);
+        //aggiungo al div .message la classe presente
+        nuovo_messaggio.addClass('sent');
+        //inserisco il messaggio all'interno del contanaier
+        $('.right-messages.active').append(nuovo_messaggio);
+        //resetto l'input con una stringa vuota
+        $('.new-message-inputs').val('');
+
+    }
+}
+
+//Questa funzione restituisce automaticamente una risposta con la stringa ok
+function answer() {
+        //clono il template
+        var nuovo_messaggio = $('.template .message').clone();
+        //inserisco nel giusto span il testo stringa OK
+        nuovo_messaggio.children('.message-text').text('OK');
+        //aggiungo al div .message la classe presente
+        nuovo_messaggio.addClass('received');
+        //inserisco il messaggio all'interno del contanaier
+        $('.right-messages.active').append(nuovo_messaggio);
+}
